614901ddb55d3fadece0023127dfabfc91b1d57b
Paul Walter <pwalter@ola.org.uk>
2022-12-13 09:43:14 +0000

    Only three images visible at any time

--- memory.py
+++ memory.py
@@ -7,28 +7,41 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
 
+selected_cells = []
 
-
+def handle_click(instance):
+    print(len(selected_cells))
+    if len(selected_cells) < 3:
+        instance.parentCell.reveal_image()  
 
 class Cell(BoxLayout):
     def __init__(self, image_id):
         super().__init__()
+        self.imageId = image_id
+        self.isDisabled = False
         image_location = 'assets/image{0}.png'.format(image_id)
         self.imageView = Image(source=image_location)
         self.buttonView = Button()
-        self.buttonView.bind(on_press=self.reveal_image)
+        self.buttonView.bind(on_press=handle_click)
+        self.buttonView.parentCell = self
         self.add_widget(self.buttonView)
 
-    def reveal_image(self, instance):
+    def reveal_image(self):
         self.remove_widget(self.buttonView)
         self.add_widget(self.imageView)
-        Clock.schedule_once(self.hide_image, 2)
+        self.isDisabled = True
+        selected_cells.append(self)
+        Clock.schedule_once(self.hide_image, 5)
 
     def hide_image(self, instance):
         self.remove_widget(self.imageView)
         self.add_widget(self.buttonView)
+        selected_cells.remove(self)
+        self.isDisabled = False
+
 
 class Application(App):
+
     def build(self):
         layout = GridLayout(cols=4)
         for row in range(4):
